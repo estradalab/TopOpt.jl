97ad2dc26a1d68aa9142d522c6693387cb40c61e
DennisTheEngineer <73843982+DennisTheEngineer@users.noreply.github.com>
2024-10-30 11:38:39 -0400

    Sensitivity Code and Hyperelastic Models (#10)
    * Debug Mooney-Rivlin consitutive law and decoupled goodness
    
    * Created new branch with goodness metric calcs; Added new hyperelasticity functions

--- src/TopOptProblems/constitutive_models.jl
+++ src/TopOptProblems/constitutive_models.jl
@@ -2,22 +2,36 @@ abstract type ConstitutiveLaw end
 
 @params struct NeoHooke{T} <: ConstitutiveLaw
     μ::T
-    λ::T
+    κ::T
 end
 @params struct MooneyRivlin{T} <: ConstitutiveLaw
     C₁₀::T
     C₀₁::T
     κ::T
 end
+@params struct Yeoh{T,S} <: ConstitutiveLaw
+    Cᵢ₀::T
+    κ::S
+end
+@params struct ArrudaBoyce{T} <: ConstitutiveLaw
+    μ::T
+    λₘ::T
+    κ::T
+end
+# @params struct Ogden{T,S} <: ConstitutiveLaw
+#     μᵢ::T
+#     αᵢ::T
+#     κ::S
+# end
 
 function NeoHooke(ξ::Dict{Symbol,<:Real})
     μ = ξ[:μ]
-    λ = ξ[:λ]
-    _T = promote_type(typeof(μ), typeof(λ))
+    κ = ξ[:κ]
+    _T = promote_type(typeof(μ), typeof(κ))
     T = _T <: Integer ? Float64 : _T
-    return NeoHooke(T(μ),T(λ))
+    return NeoHooke(T(μ),T(κ))
 end
-function MooneyRivlin(ξ::Dict{Symbol,AbstractFloat})
+function MooneyRivlin(ξ::Dict{Symbol,<:Real})
     C₁₀ = ξ[:C₁₀]
     C₀₁ = ξ[:C₀₁]
     κ = ξ[:κ]
@@ -25,3 +39,30 @@ function MooneyRivlin(ξ::Dict{Symbol,AbstractFloat})
     T = _T <: Integer ? Float64 : _T
     return MooneyRivlin(T(C₁₀),T(C₀₁),T(κ))
 end
+function Yeoh(ξ::Dict{Symbol,<:Any})
+    Cᵢ₀ = ξ[:Cᵢ₀]
+    κ = ξ[:κ]
+    _T = promote_type(typeof(Cᵢ₀))
+    T = _T <: Integer ? Float64 : _T
+    _S = promote_type(typeof(κ))
+    S = _T <: Integer ? Float64 : _S
+    return Yeoh(T(Cᵢ₀),S(κ))
+end
+function ArrudaBoyce(ξ::Dict{Symbol,<:Real})
+    μ = ξ[:μ]
+    λₘ = ξ[:λₘ]
+    κ = ξ[:κ]
+    _T = promote_type(typeof(μ), typeof(λₘ), typeof(κ))
+    T = _T <: Integer ? Float64 : _T
+    return ArrudaBoyce(T(μ),T(λₘ),T(κ))
+end
+# function Ogden(ξ::Dict{Symbol,<:Any})
+#     μᵢ = ξ[:μᵢ]
+#     αᵢ = ξ[:αᵢ]
+#     κ = ξ[:κ]
+#     _T = promote_type(typeof(μᵢ),typeof(αᵢ))
+#     T = _T <: Integer ? Float64 : _T
+#     _S = promote_type(typeof(κ))
+#     S = _T <: Integer ? Float64 : _S
+#     return Ogden(T(μᵢ),T(αᵢ),S(κ))
+# end
